a14a9f50732a026051a055c7271d9ee1de0e4d76
edymlima <edymlima@gmail.com>
2020-06-04 00:42:01 -0300

    mundando na linha

--- PrjLista/src/br/com/list/ListName.java
+++ PrjLista/src/br/com/list/ListName.java
@@ -4,7 +4,10 @@ public class ListName {
 
 	public static void main(String[] args) {
 		// aqui vai o teste
-		System.out.println("Teste com a nova branch");
+		System.out.println("Teste linha");
+		System.out.println("Teste linha");
+		System.out.println("Teste linha");
+
 
 	}
 
