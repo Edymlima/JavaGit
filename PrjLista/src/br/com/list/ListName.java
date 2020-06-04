c93927775edd79b418460fbb642dada0e3845229
edymlima <edymlima@gmail.com>
2020-06-04 00:36:48 -0300

    Atualizado pelo git eclipse

--- PrjLista/src/br/com/list/ListName.java
+++ PrjLista/src/br/com/list/ListName.java
@@ -4,7 +4,7 @@ public class ListName {
 
 	public static void main(String[] args) {
 		// aqui vai o teste
-		System.out.println("Teste com a nova branch");
+		System.out.println("Mudado Pelo Eclipse");
 
 	}
 
